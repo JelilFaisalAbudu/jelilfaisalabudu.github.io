cdf711dfa87d5a2d29860bd3f3ff57cc3452b6be
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2020-10-11 11:58:30 +0000

    Create JS function to change the color of the current nav item

--- index.js
+++ index.js
@@ -8,3 +8,20 @@ window.onscroll = function () {
         myNav.classList.remove("navbar-bg-white");
     }
 };
+
+
+// Add active css class to the current element
+
+let navContainer = document.getElementById("nav");
+
+// Get all buttons with class="btn" inside the container
+let navLinks = navContainer.getElementsByClassName("nav-link");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (let i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click", function() {
+    let current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
