5e1418b75a6b62e84919d217e07df47fd3884192
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2020-10-11 22:01:38 +0000

    Add smooth scrolling to the page

--- index.js
+++ index.js
@@ -25,3 +25,24 @@ for (let i = 0; i < navLinks.length; i++) {
     this.className += " active";
   });
 }
+
+/* window.addEventListener('DOMContentLoaded', () => {
+
+	const observer = new IntersectionObserver(entries => {
+		entries.forEach(entry => {
+			const id = entry.target.getAttribute('id');
+			if (entry.intersectionRatio > 0) {
+				document.querySelector(`nav a[href="#${id}"]`).classList.add('active');
+			} else {
+				document.querySelector(`nav a[href="#${id}"]`).classList.remove('active');
+			}
+		});
+	});
+
+	// Track all sections that have an `id` applied
+	document.querySelectorAll('section[id]').forEach((section) => {
+		observer.observe(section);
+	});
+	
+});
+*/
